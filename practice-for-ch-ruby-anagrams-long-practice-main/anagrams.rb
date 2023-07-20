e027160fcc27c96d723a26107c14edae99a0dcaa
Dan Lee <danielhlee4@gmail.com>
2023-07-20 16:34:59 -0400

    add #second_anagram?

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -7,6 +7,23 @@ def first_anagram?(word1, word2)
     return word1_perms.include?(word2_arr)
 end
 
-p first_anagram?("gizmo", "sally")    #=> false
-p first_anagram?("elvis", "lives")    #=> true
+# p first_anagram?("gizmo", "sally")    #=> false
+# p first_anagram?("elvis", "lives")    #=> true
 
+def second_anagram?(string1, string2)
+    string1_arr = string1.split("")
+    string2_arr = string2.split("")
+
+    string1_arr.each do |char|
+        if string2_arr.find_index(char).nil?
+            return false
+        else
+            string2_arr.delete_at(string2_arr.find_index(char))
+        end
+    end
+
+    return string2_arr.length == 0
+end
+
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
